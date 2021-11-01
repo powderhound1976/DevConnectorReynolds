7c93d983a3a7210a9d86d3304ce35df88ec9d4f2
powderhound1976 <christopherreynolds@mail.weber.edu>
2021-11-01 11:47:31 -0600

    Completed section 8

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,11 @@
 import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
 import PropTypes from 'prop-types';
+import { register } from '../../actions/auth';
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
 	const [formData, setFormData] = useState({
 		name: '',
 		email: '',
@@ -22,10 +23,14 @@ const Register = ({ setAlert }) => {
 		if (password !== password2) {
 			setAlert('Passwords do not match', 'danger');
 		} else {
-			console.log('SUCCESS');
+			register({ name, email, password });
 		}
 	};
 
+	if (isAuthenticated) {
+		return <Redirect to='/dashboard' />;
+	}
+	
 	return (
 		<Fragment>
 			<h1 className='large text-primary'>Sign Up</h1>
@@ -40,7 +45,7 @@ const Register = ({ setAlert }) => {
 						name='name'
 						value={name}
 						onChange={e => onChange(e)}
-						required
+						// required
 					/>
 				</div>
 				<div className='form-group'>
@@ -50,7 +55,7 @@ const Register = ({ setAlert }) => {
 						name='email'
 						value={email}
 						onChange={e => onChange(e)}
-						required
+						// required
 					/>
 					<small className='form-text'>
 						This site uses Gravatar so if you want a profile image,
@@ -62,7 +67,7 @@ const Register = ({ setAlert }) => {
 						type='password'
 						placeholder='Password'
 						name='password'
-						minLength='6'
+						// minLength='6'
 						value={password}
 						onChange={e => onChange(e)}
 					/>
@@ -72,7 +77,7 @@ const Register = ({ setAlert }) => {
 						type='password'
 						placeholder='Confirm Password'
 						name='password2'
-						minLength='6'
+						// minLength='6'
 						value={password2}
 						onChange={e => onChange(e)}
 					/>
@@ -92,10 +97,14 @@ const Register = ({ setAlert }) => {
 };
 
 Register.propTypes = {
-	setAlert: PropTypes.func.isRequired
+	setAlert: PropTypes.func.isRequired,
+	register: PropTypes.func.isRequired,
+	isAuthenticated: PropTypes.bool,
 };
 
-export default connect(
-	null, 
-	{ setAlert }
-	)(Register);
+
+const mapStateToProps = state => ({
+	isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps, { setAlert, register })(Register);
