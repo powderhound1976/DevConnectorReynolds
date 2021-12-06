20c2bf074bd2bfaddc82feb61055ce9e46f7e93e
powderhound1976 <christopherreynolds@mail.weber.edu>
2021-12-05 19:39:39 -0700

    Able to see posts again...

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,5 +1,5 @@
-// import axios from 'axios';
-import api from '../utils/api';
+import axios from 'axios';
+// import api from '../utils/api';
 import { setAlert } from './alert';
 import {
 	GET_POSTS,
@@ -15,7 +15,7 @@ import {
 // Get posts
 export const getPosts = () => async dispatch => {
 	try {
-		const res = await api.get('/posts');
+		const res = await axios.get('/api/posts');
 
 		dispatch({
 			type: GET_POSTS,
@@ -35,7 +35,7 @@ export const getPosts = () => async dispatch => {
 // Add like
 export const addLike = id => async dispatch => {
   try {
-    const res = await api.put(`/posts/like/${id}`);
+    const res = await axios.put(`/api/posts/like/${id}`);
 
     dispatch({
       type: UPDATE_LIKES,
@@ -52,7 +52,7 @@ export const addLike = id => async dispatch => {
 // Remove like
 export const removeLike = id => async dispatch => {
   try {
-    const res = await api.put(`/posts/unlike/${id}`);
+    const res = await axios.put(`/api/posts/unlike/${id}`);
 
     dispatch({
       type: UPDATE_LIKES,
@@ -69,7 +69,7 @@ export const removeLike = id => async dispatch => {
 // Delete post
 export const deletePost = id => async dispatch => {
   try {
-    await api.delete(`/posts/${id}`);
+    await axios.delete(`/api/posts/${id}`);
 
     dispatch({
       type: DELETE_POST,
@@ -88,7 +88,7 @@ export const deletePost = id => async dispatch => {
 // Add post
 export const addPost = formData => async dispatch => {
   try {
-    const res = await api.post('/posts', formData);
+    const res = await axios.post('/api/posts', formData);
 
     dispatch({
       type: ADD_POST,
@@ -107,7 +107,7 @@ export const addPost = formData => async dispatch => {
 // Get post
 export const getPost = id => async dispatch => {
   try {
-    const res = await api.get(`/posts/${id}`);
+    const res = await axios.get(`/api/posts/${id}`);
 
     dispatch({
       type: GET_POST,
@@ -124,7 +124,7 @@ export const getPost = id => async dispatch => {
 // Add comment
 export const addComment = (postId, formData) => async dispatch => {
   try {
-    const res = await api.post(`/posts/comment/${postId}`, formData);
+    const res = await axios.post(`/api/posts/comment/${postId}`, formData);
 
     dispatch({
       type: ADD_COMMENT,
@@ -143,7 +143,7 @@ export const addComment = (postId, formData) => async dispatch => {
 // Delete comment
 export const deleteComment = (postId, commentId) => async dispatch => {
   try {
-    await api.delete(`/posts/comment/${postId}/${commentId}`);
+    await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
 
     dispatch({
       type: REMOVE_COMMENT,
--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,29 +1,29 @@
-import axios from 'axios';
-import store from '../store';
-import { LOGOUT } from '../actions/types';
+// import axios from 'axios';
+// import store from '../store';
+// import { LOGOUT } from '../actions/types';
 
-const api = axios.create({
-  baseURL: '/api',
-  headers: {
-    'Content-Type': 'application/json'
-  }
-});
-/**
- intercept any error responses from the api
- and check if the token is no longer valid.
- ie. Token has expired or user is no longer
- authenticated.
- logout the user if the token has expired
-**/
+// const api = axios.create({
+//   baseURL: '/api',
+//   headers: {
+//     'Content-Type': 'application/json'
+//   }
+// });
+// /**
+//  intercept any error responses from the api
+//  and check if the token is no longer valid.
+//  ie. Token has expired or user is no longer
+//  authenticated.
+//  logout the user if the token has expired
+// **/
 
-api.interceptors.response.use(
-  res => res,
-  err => {
-    if (err.response.status === 401) {
-      store.dispatch({ type: LOGOUT });
-    }
-    return Promise.reject(err);
-  }
-);
+// api.interceptors.response.use(
+//   res => res,
+//   err => {
+//     if (err.response.status === 401) {
+//       console.log('logout');
+//     }
+//     return Promise.reject(err);
+//   }
+// );
 
-export default api;
+// export default api;
